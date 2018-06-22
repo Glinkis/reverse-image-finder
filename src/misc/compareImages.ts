66ca716918c5cd7352ea9ff604be373cfbeff6a3
Victor Glindås <glinkis@gmail.com>
2018-06-22 12:59:18 +0200

    Fix matching.

--- src/misc/compareImages.ts
+++ src/misc/compareImages.ts
@@ -6,13 +6,12 @@ import { decodePng } from "././decodePng";
 import { decodeJpg } from "./decodeJpg";
 import { store } from "./store";
 
-const WIDTH = 512;
-const HEIGHT = 512;
+const WIDTH = 256;
+const HEIGHT = 256;
 
 const canvas = document.createElement("canvas");
 canvas.width = WIDTH;
 canvas.height = HEIGHT;
-const ctx = canvas.getContext("2d");
 
 export const compareImage = async (path: string) => {
   const imageData = await resizeImage(path);
@@ -28,21 +27,37 @@ export const compareImage = async (path: string) => {
     }
   );
 
+  console.log(imageData, match);
+
   return match / store.imageData.data.length < store.threshold;
 };
 
 export const resizeImage = async (path: string) => {
   const img = new Image();
   img.src = await parseImage(path);
+  const ctx = canvas.getContext("2d");
   ctx.drawImage(img, 0, 0, WIDTH, HEIGHT);
   return ctx.getImageData(0, 0, WIDTH, HEIGHT);
 };
 
 export const parseImage = async (image: string) => {
   switch (path.extname(image).toLowerCase()) {
-    case ".psd":
-      return await decodePsd(image);
-    default:
+    case ".jpg":
       return image;
+    case ".png":
+      return image;
+    case ".psd":
+      const psd = await decodePsd(image);
+      return "data:image/png;base64," + arrayBufferToBase64(psd.data);
   }
 };
+
+function arrayBufferToBase64(buffer) {
+  var binary = "";
+  var bytes = new Uint8Array(buffer);
+  var len = bytes.byteLength;
+  for (let i = 0; i < len; i++) {
+    binary += String.fromCharCode(bytes[i]);
+  }
+  return window.btoa(binary);
+}
--- src/misc/decodePsd.ts
+++ src/misc/decodePsd.ts
@@ -2,16 +2,5 @@ import * as PSD from "psd";
 
 export const decodePsd = async (image: string) => {
   const psd = await PSD.open(image);
-  const png = psd.image.toPng();
-  return "data:image/png;base64," + arrayBufferToBase64(png.data);
+  return psd.image.toPng();
 };
-
-function arrayBufferToBase64(buffer) {
-  var binary = "";
-  var bytes = new Uint8Array(buffer);
-  var len = bytes.byteLength;
-  for (let i = 0; i < len; i++) {
-    binary += String.fromCharCode(bytes[i]);
-  }
-  return window.btoa(binary);
-}
