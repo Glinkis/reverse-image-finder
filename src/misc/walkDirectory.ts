b9412ced08a687a0b4edff0a7a2e4dd11f135b0b
Victor Glindås <glinkis@gmail.com>
2018-06-25 21:45:40 +0200

    Improve walkDirectory.

--- src/misc/walkDirectory.ts
+++ src/misc/walkDirectory.ts
@@ -9,7 +9,11 @@ export const walkDirectory = async (dir: string) => {
   store.isSearching = true;
   let results: string[] = [];
 
-  const list = await readdirAsync(dir);
+  const list = await readdirAsync(dir).catch(console.error);
+
+  if (!list) {
+    return results;
+  }
 
   let pending = list.length;
   if (!pending) {
@@ -17,11 +21,16 @@ export const walkDirectory = async (dir: string) => {
   }
 
   for (let file of list) {
+    if (!store.isSearching) {
+      return results;
+    }
+
     file = path.resolve(dir, file);
-    const stat = await statAsync(file);
+
+    const stat = await statAsync(file).catch(console.error);
 
     if (stat && stat.isDirectory()) {
-      const result = await walkDirectory(file);
+      const result = await walkDirectory(file).catch(console.error);
       if (result) {
         results = results.concat(result);
       }
