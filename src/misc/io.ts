5159de1c52c9510e1147d492ddfbaa41ba95ce69
Victor Glindås <glinkis@gmail.com>
2018-06-25 16:02:08 +0200

    Fix testing.

--- src/misc/io.ts
+++ src/misc/io.ts
@@ -4,7 +4,10 @@ import * as path from "path";
 import { app, remote } from "electron";
 
 const indexedDir = (function createIndexDirectory() {
-  const userDataDir = (app || remote.app).getPath("userData");
+  if (!app) {
+    return;
+  }
+  const userDataDir = app.getPath("userData");
   const dir = userDataDir + "/indexed/";
   if (!fs.existsSync(dir)) fs.mkdirSync(dir);
   return dir;
@@ -13,24 +16,30 @@ const indexedDir = (function createIndexDirectory() {
 const readFileAsync = util.promisify(fs.readFile);
 
 export const readPixelData = async (name: string) => {
-  try {
-    const pixelData = await readFileAsync(path.join(indexedDir, name));
-    return pixelData as Uint8Array;
-  } catch {}
+  if (indexedDir) {
+    try {
+      const pixelData = await readFileAsync(path.join(indexedDir, name));
+      return pixelData as Uint8Array;
+    } catch {}
+  }
 };
 
 const writeFileAsync = util.promisify(fs.writeFile);
 
 export const writePixelData = async (name: string, data: Uint8Array) => {
-  await writeFileAsync(path.join(indexedDir, name), data);
+  if (indexedDir) {
+    await writeFileAsync(path.join(indexedDir, name), data);
+  }
 };
 
 const readdirAsync = util.promisify(fs.readdir);
 const unlinkAsync = util.promisify(fs.unlink);
 
 export const clearPixelData = async () => {
-  const files = await readdirAsync(indexedDir);
-  for (const file of files) {
-    await unlinkAsync(path.join(indexedDir, file));
+  if (indexedDir) {
+    const files = await readdirAsync(indexedDir);
+    for (const file of files) {
+      await unlinkAsync(path.join(indexedDir, file));
+    }
   }
 };
