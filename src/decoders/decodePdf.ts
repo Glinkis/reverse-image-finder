7debe290456d9cd8c9ad70683b083089ad99bc4f
Victor Glindås <glinkis@gmail.com>
2018-06-26 22:12:14 +0200

    Start working on a pdf decoder.

---
+++ src/decoders/decodePdf.ts
@@ -0,0 +1,19 @@
+import { PDFJS } from "pdfjs-dist";
+import { readFileAsync } from "../misc/promisified";
+
+const canvas = document.createElement("canvas");
+const canvasContext = canvas.getContext("2d") as CanvasRenderingContext2D;
+
+const decodePdf = async (image: string) => {
+  const document = await readFileAsync(image);
+  const pdf = await PDFJS.getDocument(document);
+  const page = await pdf.getPage(1);
+  const viewport = page.getViewport(1);
+
+  canvas.height = viewport.height;
+  canvas.width = viewport.width;
+
+  await page.render({ canvasContext, viewport });
+
+  return canvasContext.getImageData(0, 0, viewport.height, viewport.width);
+};
