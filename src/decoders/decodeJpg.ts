2a1973bd06f804adc960c69a1d87ef2788890863
Victor Glindås <glinkis@gmail.com>
2018-06-29 12:50:42 +0200

    Refactor decodeJpg

--- src/decoders/decodeJpg.ts
+++ src/decoders/decodeJpg.ts
@@ -1,12 +1,7 @@
 import { store } from "../misc/store";
 import { decodeWithCanvas } from "../misc/decodeWithCanvas";
 
-/**
- * {@link https://github.com/eugeneware/jpeg-js}
- */
-const decodeJpg = (image: string) => {
-  return decodeWithCanvas(image);
-};
+const decodeJpg = (image: string) => decodeWithCanvas(image);
 
 store.decoders.set(".jpg", decodeJpg);
 store.decoders.set(".jpeg", decodeJpg);
